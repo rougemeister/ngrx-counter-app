acd756e5d9842ea3c4ce4488e6b767b982169868
rougemeister <rougemeister1@gmail.com>
2024-08-20 14:33:03 +0000

    Added page structure and basic styling

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CounterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Counter App';
+}
