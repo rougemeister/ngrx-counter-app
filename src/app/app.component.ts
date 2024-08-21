90062c7f52d4b39ca55a4f20c3c81552987c8179
rougemeister <rougemeister1@gmail.com>
2024-08-21 12:59:32 +0000

    Removed interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,39 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './components/counter/counter.component';
 import { ModalService } from './services/modal.service';
-import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent, AsyncPipe],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   showCounterModal = false;
   showIntervalModal = false;
-  count$: Observable<number>;
 
- 
-
-  initialCountValue: number;
-
-  constructor(private store: Store<{ count: number }>, private modalService: ModalService) {
-    this.count$ = this.store.select('count');
-    this.initialCountValue = JSON.parse(localStorage.getItem('count') ?? '0');
-  }
-
-  closeIntervalModal() {
-    const intervalInput = document.getElementById('interval') as HTMLInputElement;
-    intervalInput.value = this.initialCountValue.toString();
-    this.showIntervalModal = false;
-  }
-
-
-
+  constructor(private modalService: ModalService) {}
 
   ngOnInit(): void {
     this.modalService.showCounterModal$.subscribe(show => {
@@ -50,5 +30,8 @@ export class AppComponent {
     this.modalService.closeCounterModal();
   }
 
- 
+  closeIntervalModal() {
+    this.modalService.closeIntervalModal();
+  }
+
 }
